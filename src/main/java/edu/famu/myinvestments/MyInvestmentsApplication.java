4cd6fbf70256dc45a65b4ab55b1b728798e55100
kyB1001 <59378562+kyB1001@users.noreply.github.com>
2021-11-23 10:35:29 -0800

    Code base
    This is the framework for the spring application. If you were to run the application it should allow you to listen on port 8080.
    
    I put @cwilliams70  and @Neel0319  as CoAuthors. So, you should be able to pull and push code to the repository.
    
    NEEDED: - Login Page
         - Basic Home Page
    Co-Authored-By: Neel0319 <65676564+Neel0319@users.noreply.github.com>
    Co-Authored-By: Chris Williams <57336741+cwilliams70@users.noreply.github.com>

---
+++ src/main/java/edu/famu/myinvestments/MyInvestmentsApplication.java
@@ -0,0 +1,13 @@
+package edu.famu.myinvestments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInvestmentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyInvestmentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/famu/myinvestments/MyInvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.famu.myinvestments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyInvestmentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
